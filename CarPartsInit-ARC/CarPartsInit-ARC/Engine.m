6fdbc79d995f5a28c93095682f65faaa4eac906b
CrazycodingIOS <62689154+CrazycodingIOS@users.noreply.github.com>
2020-11-26 21:38:52 +0800

    Create Engine.m

---
+++ CarPartsInit-ARC/CarPartsInit-ARC/Engine.m
@@ -0,0 +1,17 @@
+//
+//  Engine.m
+//  CarPartsInit-ARC
+//
+//  Created by 全先帅 on 2020/11/26.
+//
+
+#import "Engine.h"
+
+@implementation Engine
+
+-(NSString*)description
+{
+    return (@"I am an Engine, Vrooom!");
+}
+
+@end
