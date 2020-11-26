ffbe2d8d1ce14ed1e7a507a1fe5b3a907d5bd802
CrazycodingIOS <62689154+CrazycodingIOS@users.noreply.github.com>
2020-11-26 21:39:02 +0800

    Create Slant6.h

---
+++ CarPartsInit-ARC/CarPartsInit-ARC/Slant6.h
@@ -0,0 +1,17 @@
+//
+//  Slant6.h
+//  CarPartsInit-ARC
+//
+//  Created by 全先帅 on 2020/11/26.
+//
+
+#import <Foundation/Foundation.h>
+#import "Engine.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Slant6 : Engine
+
+@end
+
+NS_ASSUME_NONNULL_END
