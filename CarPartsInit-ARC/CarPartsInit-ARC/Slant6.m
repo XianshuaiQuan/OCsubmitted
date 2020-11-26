6ab9bce30f42dae2f0b6f28112d9599e18386a72
CrazycodingIOS <62689154+CrazycodingIOS@users.noreply.github.com>
2020-11-26 21:39:06 +0800

    Create Slant6.m

---
+++ CarPartsInit-ARC/CarPartsInit-ARC/Slant6.m
@@ -0,0 +1,16 @@
+//
+//  Slant6.m
+//  CarPartsInit-ARC
+//
+//  Created by 全先帅 on 2020/11/26.
+//
+
+#import "Slant6.h"
+
+@implementation Slant6
+
+-(NSString*)description
+{
+    return @"I am a Slant6!";
+}
+@end
