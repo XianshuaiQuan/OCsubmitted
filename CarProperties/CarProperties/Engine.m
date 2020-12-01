aac2ce28e9607d296ed0e5177d4df715b3889e6b
CrazycodingIOS <62689154+CrazycodingIOS@users.noreply.github.com>
2020-12-01 17:42:36 +0800

    Create Engine.m

---
+++ CarProperties/CarProperties/Engine.m
@@ -0,0 +1,18 @@
+//
+//  Engine.m
+//  CarProperties
+//
+//  Created by 全先帅 on 2020/12/1.
+//
+
+#import "Engine.h"
+
+@implementation Engine
+
+-(NSString*) description
+{
+    NSString* str;
+    str=[NSString localizedStringWithFormat:@"I am an engine!"];
+    return str;
+}
+@end
