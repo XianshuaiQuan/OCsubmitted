129ef0077367641a968fe8eed2d8ef8721bff317
CrazycodingIOS <62689154+CrazycodingIOS@users.noreply.github.com>
2020-12-01 17:42:43 +0800

    Create Slant6.m

---
+++ CarProperties/CarProperties/Slant6.m
@@ -0,0 +1,18 @@
+//
+//  Slant6.m
+//  CarProperties
+//
+//  Created by 全先帅 on 2020/12/1.
+//
+
+#import "Slant6.h"
+
+@implementation Slant6
+
+-(NSString*) description
+{
+    NSString* str;
+    str=[NSString stringWithFormat:@"I am a slant-6!"];
+    return str;
+}
+@end
