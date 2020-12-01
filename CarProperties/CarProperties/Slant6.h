07befdae19cd546941f89b801739c123a9057dae
CrazycodingIOS <62689154+CrazycodingIOS@users.noreply.github.com>
2020-12-01 17:42:40 +0800

    Create Slant6.h

---
+++ CarProperties/CarProperties/Slant6.h
@@ -0,0 +1,16 @@
+//
+//  Slant6.h
+//  CarProperties
+//
+//  Created by 全先帅 on 2020/12/1.
+//
+
+#import "Engine.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Slant6 : Engine
+
+@end
+
+NS_ASSUME_NONNULL_END
