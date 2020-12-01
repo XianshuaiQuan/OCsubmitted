c7ba77480ab39e0b584e3feb1b24d204977f8460
CrazycodingIOS <62689154+CrazycodingIOS@users.noreply.github.com>
2020-12-01 17:42:20 +0800

    Create AllWeatherRadial.h

---
+++ CarProperties/CarProperties/AllWeatherRadial.h
@@ -0,0 +1,19 @@
+//
+//  AllWeatherRadial.h
+//  CarProperties
+//
+//  Created by 全先帅 on 2020/12/1.
+//
+
+#import "Tire.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AllWeatherRadial : Tire
+
+@property float rainHandling;
+@property float snowHandling;
+
+@end
+
+NS_ASSUME_NONNULL_END
