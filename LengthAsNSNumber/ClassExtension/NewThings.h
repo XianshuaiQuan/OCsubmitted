5a9f056d51d4b61489cc480ff22f3d8d7d3bff6e
CrazycodingIOS <62689154+CrazycodingIOS@users.noreply.github.com>
2020-12-04 23:20:45 +0800

    Create NewThings.h

---
+++ LengthAsNSNumber/ClassExtension/NewThings.h
@@ -0,0 +1,16 @@
+//
+//  NewThings.h
+//  ClassExtension
+//
+//  Created by 全先帅 on 2020/12/4.
+//
+
+#import "Things.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NewThings : Things
+
+@end
+
+NS_ASSUME_NONNULL_END
