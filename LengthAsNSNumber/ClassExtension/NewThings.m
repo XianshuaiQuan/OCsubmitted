44f24a2de9feff3cd2ebfb0b58c7905dc3b375ac
CrazycodingIOS <62689154+CrazycodingIOS@users.noreply.github.com>
2020-12-04 23:21:08 +0800

    Create NewThings.m

---
+++ LengthAsNSNumber/ClassExtension/NewThings.m
@@ -0,0 +1,17 @@
+//
+//  NewThings.m
+//  ClassExtension
+//
+//  Created by 全先帅 on 2020/12/4.
+//
+
+#import "NewThings.h"
+
+@implementation NewThings
+
+-(void) addNumber
+{
+    self.things1=[NSNumber numberWithInteger:20];
+}
+
+@end
