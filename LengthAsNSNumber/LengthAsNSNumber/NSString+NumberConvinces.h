23ebd5c553cc00aea0fc55da8f7f3a35124ca078
CrazycodingIOS <62689154+CrazycodingIOS@users.noreply.github.com>
2020-12-04 23:21:28 +0800

    Create NSString+NumberConvinces.h

---
+++ LengthAsNSNumber/LengthAsNSNumber/NSString+NumberConvinces.h
@@ -0,0 +1,18 @@
+//
+//  NSString+NumberConvinces.h
+//  LengthAsNSNumber
+//
+//  Created by 全先帅 on 2020/12/4.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (NumberConvinces)
+
+-(NSNumber*)  lengthAsNumber;
+
+@end//NumberConvinces
+
+NS_ASSUME_NONNULL_END
