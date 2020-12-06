96e783484ad81eeee54705231d0844baa8a5a59d
CrazycodingIOS <62689154+CrazycodingIOS@users.noreply.github.com>
2020-12-06 15:01:26 +0800

    Create Thing3.m

---
+++ CategoryThing/CategoryThing/Thing3.m
@@ -0,0 +1,22 @@
+//
+//  Thing3.m
+//  CategoryThing
+//
+//  Created by 全先帅 on 2020/12/6.
+//
+
+#import <Foundation/Foundation.h>
+#import "CategoryThing.h"
+
+@implementation CategoryThing(Thing3)
+
+-(void) setThing3:(NSInteger)thing3
+{
+    _thing3=thing3;
+}
+-(NSInteger) thing3
+{
+    return _thing3;
+}
+
+@end
